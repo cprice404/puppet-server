f1530902a0c1c803aa4f0e367f034fe825c2b6bb
Chris Price <chris@puppetlabs.com>
2016-01-09 15:03:41 -0800

    Switch to 'context' for v4-app

--- test/unit/puppetlabs/comidi_pdb_scratch.clj
+++ test/unit/puppetlabs/comidi_pdb_scratch.clj
@@ -1,7 +1,8 @@
 (ns puppetlabs.comidi-pdb-scratch
   (:require [clojure.test :refer :all]
             [puppetlabs.comidi :as cmdi]
-            [ring.mock.request :as mock]))
+            [ring.mock.request :as mock]
+            [puppetlabs.comidi :as comidi]))
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; Fake impls of real PDB functions, just to test routes
@@ -135,45 +136,47 @@
         #(wrap-with-parent-check'' % version :node :node)))}}))
 
 (def v4-app
-  {;"" (experimental-index-app version)
-   "/facts" (facts-app version)
-   ;"/edges" (comp (query-handler version)
-   ;               restrict-query-to-active-nodes
-   ;               #(restrict-query-to-entity "edges" %)
-   ;               (extract-query' {:optional query-params}))
-   ;"/factsets" (factset-app version)
-   ;"/fact-names" (fact-names-app version)
-   ;"/fact-contents"   (comp (query-handler version)
-   ;                         #(restrict-query-to-entity "fact_contents" %)
-   ;                         restrict-query-to-active-nodes
-   ;                         (extract-query' {:optional query-params}))
-   ;"/fact-paths" (create-paging-query-handler "fact_paths")
+  [""
+   {;"" (experimental-index-app version)
+    "/facts" (facts-app version)
+    ;"/edges" (comp (query-handler version)
+    ;               restrict-query-to-active-nodes
+    ;               #(restrict-query-to-entity "edges" %)
+    ;               (extract-query' {:optional query-params}))
+    ;"/factsets" (factset-app version)
+    ;"/fact-names" (fact-names-app version)
+    ;"/fact-contents"   (comp (query-handler version)
+    ;                         #(restrict-query-to-entity "fact_contents" %)
+    ;                         restrict-query-to-active-nodes
+    ;                         (extract-query' {:optional query-params}))
+    ;"/fact-paths" (create-paging-query-handler "fact_paths")
 
-   "/nodes" (node-app version)
-   ;"/environments" (environments-app version)
-   ;
-   ;
-   ;"/resources" (resources-app version)
-   ;"/catalogs" (catalog-app version)
-   ;"/events" (events-app version)
-   ;"/event-counts" (create-query-handler "event_counts" {:required ["summarize_by"]
-   ;                                                      :optional (concat ["counts_filter" "count_by"
-   ;                                                                         "distinct_resources" "distinct_start_time"
-   ;                                                                         "distinct_end_time"]
-   ;                                                                        query-params)})
-   ;"/aggregate-event-counts" (create-query-handler "aggregate_event_counts"
-   ;                                                {:required ["summarize_by"]
-   ;                                                 :optional ["query" "counts_filter" "count_by"
-   ;                                                            "distinct_resources" "distinct_start_time"
-   ;                                                            "distinct_end_time"]})
-   ;"/reports" (reports-app version)
-   })
+    "/nodes" (node-app version)
+    ;"/environments" (environments-app version)
+    ;
+    ;
+    ;"/resources" (resources-app version)
+    ;"/catalogs" (catalog-app version)
+    ;"/events" (events-app version)
+    ;"/event-counts" (create-query-handler "event_counts" {:required ["summarize_by"]
+    ;                                                      :optional (concat ["counts_filter" "count_by"
+    ;                                                                         "distinct_resources" "distinct_start_time"
+    ;                                                                         "distinct_end_time"]
+    ;                                                                        query-params)})
+    ;"/aggregate-event-counts" (create-query-handler "aggregate_event_counts"
+    ;                                                {:required ["summarize_by"]
+    ;                                                 :optional ["query" "counts_filter" "count_by"
+    ;                                                            "distinct_resources" "distinct_start_time"
+    ;                                                            "distinct_end_time"]})
+    ;"/reports" (reports-app version)
+    }])
 
 (def routes
-  ["" {;"/v1" [[true (refuse-retired-api "v1")]]
+  #_["" {;"/v1" [[true (refuse-retired-api "v1")]]
        ;"/v2" [[true (refuse-retired-api "v2")]]
        ;"/v3" [[true (refuse-retired-api "v3")]]
-       "/v4" v4-app}])
+       "/v4" v4-app}]
+  (comidi/context "/v4" v4-app))
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; Tests / test utils
