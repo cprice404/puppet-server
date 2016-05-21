c7d170c0c168ca5a3ce045906bcecca270ef01b3
Chris Price <chris@puppetlabs.com>
2016-05-21 02:00:15 -0700

    WIP: testing with 9.1.1.0

--- src/ruby/jruby9k_scratch/server_1357/run_mvn_jrubies.sh
+++ src/ruby/jruby9k_scratch/server_1357/run_mvn_jrubies.sh
@@ -8,10 +8,14 @@ set -e
 
 mvn clean
 mvn -P jruby17 compile
-mvn -P jruby17 exec:java -Dexec.mainClass=jruby9k.Jruby9kJsonBenchmarkDriver -Dexec.args="json/pure"
-mvn -P jruby17 exec:java -Dexec.mainClass=jruby9k.Jruby9kJsonBenchmarkDriver -Dexec.args="json/ext"
+mvn -P jruby17 exec:java -Dexec.mainClass=jruby9k.Jruby9kJsonBenchmarkDriver -Dexec.args="json/pure off"
+mvn -P jruby17 exec:java -Dexec.mainClass=jruby9k.Jruby9kJsonBenchmarkDriver -Dexec.args="json/pure on"
+mvn -P jruby17 exec:java -Dexec.mainClass=jruby9k.Jruby9kJsonBenchmarkDriver -Dexec.args="json/ext off"
+mvn -P jruby17 exec:java -Dexec.mainClass=jruby9k.Jruby9kJsonBenchmarkDriver -Dexec.args="json/ext on"
 
 mvn clean
 mvn -P jruby9k compile
-mvn -P jruby9k exec:java -Dexec.mainClass=jruby9k.Jruby9kJsonBenchmarkDriver -Dexec.args="json/pure"
-mvn -P jruby9k exec:java -Dexec.mainClass=jruby9k.Jruby9kJsonBenchmarkDriver -Dexec.args="json/ext"
+mvn -P jruby9k exec:java -Dexec.mainClass=jruby9k.Jruby9kJsonBenchmarkDriver -Dexec.args="json/pure off"
+mvn -P jruby9k exec:java -Dexec.mainClass=jruby9k.Jruby9kJsonBenchmarkDriver -Dexec.args="json/pure on"
+mvn -P jruby9k exec:java -Dexec.mainClass=jruby9k.Jruby9kJsonBenchmarkDriver -Dexec.args="json/ext off"
+mvn -P jruby9k exec:java -Dexec.mainClass=jruby9k.Jruby9kJsonBenchmarkDriver -Dexec.args="json/ext on"
