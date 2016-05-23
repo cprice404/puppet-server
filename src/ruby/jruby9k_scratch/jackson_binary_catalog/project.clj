ed7379fc9de7673891807996859417c69a73e2cc
Chris Price <chris@puppetlabs.com>
2016-05-22 19:13:16 -0700

    WIP: code to parse binary content out of catalog

--- src/ruby/jruby9k_scratch/jackson_binary_catalog/project.clj
+++ src/ruby/jruby9k_scratch/jackson_binary_catalog/project.clj
@@ -1,6 +1,8 @@
 (defproject puppetlabs.scratch.jruby9k/jackson-binary-catalog "0.0.1"
   :dependencies [[org.clojure/clojure "1.7.0"]
-                 [cheshire "5.6.1"]]
+                 [cheshire "5.6.1"]
+                 [commons-io "2.5"]
+                 [commons-lang "2.6"]]
 
   :pedantic? :abort
 
