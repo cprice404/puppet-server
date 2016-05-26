a92da0b2effcf9aa3c90405d44c7f5cd95b05f92
Chris Price <chris@puppetlabs.com>
2016-05-26 16:55:56 -0700

    WIP: cleanu of serializer

--- src/ruby/jruby9k_scratch/jackson_binary_catalog/src/java/jruby_9k_scratch/MyCustomSerializer.java
+++ src/ruby/jruby9k_scratch/jackson_binary_catalog/src/java/jruby_9k_scratch/MyCustomSerializer.java
@@ -9,14 +9,11 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.SerializerProvider;
 import com.fasterxml.jackson.databind.module.SimpleModule;
 import com.fasterxml.jackson.databind.ser.std.StdScalarSerializer;
-import com.fasterxml.jackson.databind.ser.std.StdSerializer;
 import org.apache.commons.io.IOUtils;
 import org.apache.commons.io.output.ByteArrayOutputStream;
 
 import java.io.*;
-import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 
 public class MyCustomSerializer extends StdScalarSerializer<String> {
@@ -24,16 +21,14 @@ public class MyCustomSerializer extends StdScalarSerializer<String> {
         super(String.class);
     }
 
-    public static class MyUTF8JsonGenerator extends UTF8JsonGenerator {
-
-
+    public static class MyUnencodedJsonGenerator extends UTF8JsonGenerator {
         private final static byte BYTE_QUOTE = (byte) '"';
 
-        public MyUTF8JsonGenerator(IOContext ctxt, int features, ObjectCodec codec, OutputStream out) {
+        public MyUnencodedJsonGenerator(IOContext ctxt, int features, ObjectCodec codec, OutputStream out) {
             super(ctxt, features, codec, out);
         }
 
-        public MyUTF8JsonGenerator(IOContext ctxt, int features, ObjectCodec codec, OutputStream out, byte[] outputBuffer, int outputOffset, boolean bufferRecyclable) {
+        public MyUnencodedJsonGenerator(IOContext ctxt, int features, ObjectCodec codec, OutputStream out, byte[] outputBuffer, int outputOffset, boolean bufferRecyclable) {
             super(ctxt, features, codec, out, outputBuffer, outputOffset, bufferRecyclable);
             throw new IllegalStateException("Not supported!");
         }
@@ -41,39 +36,11 @@ public class MyCustomSerializer extends StdScalarSerializer<String> {
         @Override
         public void writeBinary(Base64Variant b64variant, byte[] data, int offset, int len) throws IOException, JsonGenerationException {
             throw new IllegalStateException("Not supported!");
-////            super.writeBinary(b64variant, data, offset, len);
-//            System.out.println("OUTPUT BUFFER LENGTH:" + _outputEnd);
-//            _verifyValueWrite(WRITE_BINARY);
-//            // Starting quotes
-//            if (_outputTail >= _outputEnd) {
-//                _flushBuffer();
-//            }
-//            _outputBuffer[_outputTail++] = BYTE_QUOTE;
-//
-////            while ()
-////
-////
-////            _outputBuffer[_outputTail++] = ':';
-////            _outputBuffer[_outputTail++] = ' ';
-////            _outputBuffer[_outputTail++] = '"';
-////            _outputBuffer[_outputTail++] = 'f';
-////            _outputBuffer[_outputTail++] = 'o';
-////            _outputBuffer[_outputTail++] = 'o';
-////            _outputBuffer[_outputTail++] = '"';
-////            _flushBuffer();
-//
-//
-//            // and closing quotes
-//            if (_outputTail >= _outputEnd) {
-//                _flushBuffer();
-//            }
-//            _outputBuffer[_outputTail++] = BYTE_QUOTE;
         }
 
         @Override
         public int writeBinary(Base64Variant b64variant, InputStream data, int dataLength) throws IOException, JsonGenerationException {
             //            super.writeBinary(b64variant, data, offset, len);
-            System.out.println("OUTPUT BUFFER LENGTH:" + _outputEnd);
             _verifyValueWrite(WRITE_BINARY);
             // Starting quotes
             if (_outputTail >= _outputEnd) {
@@ -86,15 +53,7 @@ public class MyCustomSerializer extends StdScalarSerializer<String> {
             int bytes;
 
             try {
-                if (dataLength < 0) { // length unknown
-                    bytes = _writeUnencodedBinary(data, encodingBuffer);
-//                } else {
-//                    int missing = _writeUnencodedBinary(data, encodingBuffer, dataLength);
-//                    if (missing > 0) {
-//                        _reportError("Too few bytes available: missing " + missing + " bytes (out of " + dataLength + ")");
-//                    }
-//                    bytes = dataLength;
-                }
+                bytes = _writeUnencodedBinary(data, encodingBuffer);
             } finally {
                 _ioContext.releaseBase64Buffer(encodingBuffer);
             }
@@ -138,7 +97,11 @@ public class MyCustomSerializer extends StdScalarSerializer<String> {
     public static class MyMappingJsonFactory extends MappingJsonFactory {
         @Override
         protected JsonGenerator _createUTF8Generator(OutputStream out, IOContext ctxt) throws IOException {
-            MyUTF8JsonGenerator gen = new MyUTF8JsonGenerator(ctxt,
+            throw new IllegalStateException("No UTF8 here, buddy.");
+        }
+
+        protected JsonGenerator _createUnencodedGenerator(OutputStream out, IOContext ctxt) throws IOException {
+            MyUnencodedJsonGenerator gen = new MyUnencodedJsonGenerator(ctxt,
                     _generatorFeatures, _objectCodec, out);
             if (_characterEscapes != null) {
                 gen.setCharacterEscapes(_characterEscapes);
@@ -153,8 +116,6 @@ public class MyCustomSerializer extends StdScalarSerializer<String> {
         /** TODO: may not need to override the stuff below, may be that
          *  overriding _createUTF8Generator is enough **/
 
-
-
         @Override
         public JsonGenerator createGenerator(OutputStream out, JsonEncoding enc) throws IOException {
 //            return super.createGenerator(out, enc);
@@ -162,7 +123,7 @@ public class MyCustomSerializer extends StdScalarSerializer<String> {
             IOContext ctxt = _createContext(out, false);
             ctxt.setEncoding(enc);
             if (enc == JsonEncoding.UTF8) {
-                return _createUTF8Generator(_decorate(out, ctxt), ctxt);
+                return _createUnencodedGenerator(_decorate(out, ctxt), ctxt);
             } else {
                 throw new IllegalStateException("In the world of Jackpson, we expect everyone to say they're UTF-8 even though they are not.");
             }
@@ -190,7 +151,6 @@ public class MyCustomSerializer extends StdScalarSerializer<String> {
     }
 
     public static void main(String[] args) throws IOException {
-//        ObjectMapper mapper = new ObjectMapper();
         ObjectMapper mapper = new ObjectMapper(new MyMappingJsonFactory());
         SimpleModule testModule = new SimpleModule("MyModule", new Version(1, 0, 0, null, "foo", "foo"));
         testModule.addSerializer(new MyCustomSerializer()); // assuming serializer declares correct class to bind to
@@ -206,8 +166,6 @@ public class MyCustomSerializer extends StdScalarSerializer<String> {
 
     @Override
     public void serialize(String value, JsonGenerator gen, SerializerProvider provider) throws IOException {
-//        gen.writeString(value);
-        System.out.println("Can write binary?: '" + gen.canWriteBinaryNatively() + "'");
         int numBytes = 10000;
         byte[] bytes =  new byte[numBytes];
         for (int i = 0; i < 10000; i++) {
