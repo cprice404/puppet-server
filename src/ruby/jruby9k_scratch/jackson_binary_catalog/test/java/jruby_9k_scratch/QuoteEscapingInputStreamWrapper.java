3fa42b1fa5485801fc6d0a63aa53453d65b81f3c
Chris Price <chris@puppetlabs.com>
2016-05-30 10:46:19 -0700

    WIP: minor cleanup

--- src/ruby/jruby9k_scratch/jackson_binary_catalog/test/java/jruby_9k_scratch/QuoteEscapingInputStreamWrapper.java
+++ src/ruby/jruby9k_scratch/jackson_binary_catalog/test/java/jruby_9k_scratch/QuoteEscapingInputStreamWrapper.java
@@ -2,10 +2,40 @@ package jruby_9k_scratch;
 
 import puppetlabs.jackson.unencoded.InputStreamWrapper;
 
+import java.io.IOException;
 import java.io.InputStream;
 
 public class QuoteEscapingInputStreamWrapper implements InputStreamWrapper {
     public InputStream wrap(InputStream inputStream) {
         return new EscapedQuoteInputStream(inputStream);
+    }
+
+    public static class EscapedQuoteInputStream extends InputStream {
+
+        private final InputStream orig;
+        private int b = -1;
+        private int tmp;
+
+        public EscapedQuoteInputStream(InputStream orig) {
+            this.orig = orig;
+        }
+
+        @Override
+        public int read() throws IOException {
+            if (b != -1) {
+                tmp = b;
+                b = -1;
+                return tmp;
+            }
+
+            b = orig.read();
+            if (b != '"') {
+                tmp = b;
+                b = -1;
+                return tmp;
+            } else {
+                return '\\';
+            }
+        }
     }
 }
