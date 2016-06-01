a58b2070e80069cf08be6becf9b0dc86a09da02d
Chris Price <chris@puppetlabs.com>
2016-06-01 07:19:34 -0700

    WIP: working on pson deserializer, need to convert ascii chars to int values

--- src/ruby/jruby9k_scratch/jackson_binary_catalog/src/java/puppetlabs/jackson/pson/PsonDecodingInputStreamWrapper.java
+++ src/ruby/jruby9k_scratch/jackson_binary_catalog/src/java/puppetlabs/jackson/pson/PsonDecodingInputStreamWrapper.java
@@ -4,6 +4,8 @@ import puppetlabs.jackson.unencoded.InputStreamWrapper;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.HashMap;
+import java.util.Map;
 
 public class PsonDecodingInputStreamWrapper implements InputStreamWrapper {
     public InputStream wrap(InputStream inputStream) {
@@ -12,9 +14,25 @@ public class PsonDecodingInputStreamWrapper implements InputStreamWrapper {
 
     public static class PsonDecodedInputStream extends InputStream {
 
+        private static final int INT_QUOTE = '"';
+        private static final int INT_BACKSLASH = '\\';
+
+        private static final Map<Integer, Integer> REPLACEMENTS = new HashMap<>();
+        static {
+            REPLACEMENTS.put((int)'b', (int)'\b');
+            REPLACEMENTS.put((int)'t', (int)'\t');
+            REPLACEMENTS.put((int)'n', (int)'\n');
+            REPLACEMENTS.put((int)'f', (int)'\f');
+            REPLACEMENTS.put((int)'r', (int)'\r');
+            REPLACEMENTS.put(INT_QUOTE, INT_QUOTE);
+            REPLACEMENTS.put(INT_BACKSLASH, INT_BACKSLASH);
+        };
+
         private final InputStream orig;
-        private int curByte;
-        private int peekByte = -1;
+        private int b;
+        private byte[] psonBuffer = new byte[4];
+        private int psonBufferSize = -1;
+        private int psonBufferOffset = -1;
 
 
         public PsonDecodedInputStream(InputStream orig) {
@@ -23,28 +41,52 @@ public class PsonDecodingInputStreamWrapper implements InputStreamWrapper {
 
         @Override
         public int read() throws IOException {
-            if (peekByte != -1) {
-                curByte = peekByte;
-                peekByte = -1;
-            } else {
-                curByte = orig.read();
-            }
-
-            if (curByte == -1) {
-                return -1;
-            }
-
-            if (curByte == '\\') {
-                peekByte = orig.read();
-                if (peekByte == '"') {
-                    peekByte = -1;
-                    return '"';
+            if (psonBufferOffset != -1) {
+                psonBufferOffset++;
+                if (psonBufferOffset >= psonBufferSize) {
+                    psonBufferOffset = -1;
                 } else {
-                    return curByte;
+                    return psonBuffer[psonBufferOffset];
                 }
             }
 
-            return curByte;
+            b = orig.read();
+            if (b == -1) {
+                return -1;
+            } else if (b == '\\') {
+                return readEscapedByte();
+            } else {
+                return b;
+            }
+        }
+
+        private int readEscapedByte() throws IOException {
+            b = orig.read();
+            if (REPLACEMENTS.containsKey(b)) {
+                return REPLACEMENTS.get(b);
+            } else if (b == 'u') {
+                return readUnicodeByte();
+            } else {
+                psonBuffer[0] = INT_BACKSLASH;
+                psonBuffer[1] = (byte)b;
+                psonBufferSize = 2;
+                psonBufferOffset = 0;
+                return psonBuffer[psonBufferOffset];
+            }
+        }
+
+        private int readUnicodeByte() throws IOException {
+            orig.read(psonBuffer, 0, 4);
+            System.out.println("READING UNICODE BYTE; FILLED PSON BUFFER:");
+            for (int i = 0; i < 4; i++) {
+                System.out.println("\tpsonBuffer[" + i + "]: " + psonBuffer[i]);
+            }
+            // we know that during encoding we only went up to 0x1f, so
+            // we can assume the integer will only require one byte to represent.
+            b = ((psonBuffer[0] & 0xff) << 24) | ((psonBuffer[1] & 0xff) << 16) |
+                    ((psonBuffer[2] & 0xff) << 8)  | (psonBuffer[3] & 0xff);
+            System.out.println("Bitwise math yielded: " + b);
+            return b;
         }
     }
 }
